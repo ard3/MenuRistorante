e15d6f5da08685b887ec4195d793c05b48d9be50
ardit <arditllaha@gmail.com>
2021-05-06 12:26:32 +0200

    Initial commit

---
+++ MenuRistorante/Data/PiattiDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MenuRistorante.Data
+{
+    public class PiattiDatabase : DbContext
+    {
+        public DbSet<PiattiRistorante> TabellaPiatti { get; set; }
+
+        public PiattiDatabase(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ MenuRistorante/Data/PiattiRistorante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MenuRistorante.Data
+{
+    public class PiattiRistorante
+    {
+        /// <summary>Classe che descrive la struttura dati di un Piatto.</summary>
+
+        [Key]
+        public int IdPiatto { get; set; }
+
+        [Required(ErrorMessage = "Devi nominare il piatto ! ")]
+        public string NomePiatto { get; set; }
+
+        [Required(ErrorMessage = "Devi selezionare il tipo di piatto ! ")]
+        public string TipoPiatto { get; set; }
+
+        [Required(ErrorMessage = "Devi selezionare un tipo di allergeno ! ")]
+        public string AllergeniContenuti { get; set; }
+
+        [Required(ErrorMessage = "Devi selezionare la stagione del piatto ! ")]
+        public string StagionePiatto { get; set; }
+
+        [Required(ErrorMessage = "Devi inserire il prezzo del piatto ! ")]
+        public decimal PrezzoPiatto { get; set; }
+
+        public string DescizionePiatto { get; set; }
+
+    }
+}
